077f653dc24b22abd8f9f123e29280dbafb08c06
Angel Contreras <angelcbenoit@gmail.com>
2018-06-11 08:42:30 -0700

    add flexbox to list of movie data in search page, also include poster image to be displayed

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -15,7 +15,7 @@ class Search extends Component {
     }
 
     componentWillMount() {
-        this.getMovieDiscoverData();
+        this.getMovieDiscoverData(); //this page will initially call this function to get movie data
     }
 
     getMovieDiscoverData() {
@@ -23,19 +23,26 @@ class Search extends Component {
         axios(url)
             .then(res => {
                 res.data.results.forEach(movie => {
-                    const joined = this.state.display.concat(movie.title);
-                    this.setState({ display: joined}, () => console.log(this.state.display));
+                    let imageLink = `https://image.tmdb.org/t/p/w200${movie.poster_path}`; //link to the poster image
+                    let HTMLMovie = (
+                        <div className="movie">
+                            <p>{movie.title}</p>
+                            <img src={imageLink} alt=""/>
+                        </div>
+                    );
+                    const joined = this.state.display.concat(HTMLMovie); //concat the state array with new list of movies
+                    this.setState({ display: joined }, () => console.log(this.state.display));
                 });
             });
     }
 
-    loadMoreDiscoverData(){
-        if(this.state.page < this.state.maxPage){
+    loadMoreDiscoverData() {
+        if (this.state.page < this.state.maxPage) { // maxpage is 1000, and if current page is less, add to it
             this.setState(prevState => {
-                return { page: prevState.page + 1}
-            }, () => console.log(this.state.page));
+                return { page: prevState.page + 1 }
+            }, () => this.getMovieDiscoverData()); // call getMovieDiscoverData to get next page of data
 
-            this.getMovieDiscoverData();
+
         }
     }
 
@@ -44,7 +51,10 @@ class Search extends Component {
         return (
             <div>
                 <h1>TEST</h1>
-                {this.state.display}
+                <div className="movieList">
+                    {this.state.display}
+                </div>
+
                 <button onClick={this.loadMoreDiscoverData}>Add more</button>
             </div>
         );
