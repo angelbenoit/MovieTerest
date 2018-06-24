0baa092d467b9dea7e6f20eb81188238287ff214
Angel Contreras <angelcbenoit@gmail.com>
2018-06-23 18:37:30 -0700

    replace a tag with NavLink from react router v4 in navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark">
-                <a className="navbar-brand" href="/">MovieTerest</a>
+                <NavLink className="navbar-brand" to="/">MovieTerest</NavLink>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav ml-auto">
-                        <a className="nav-item nav-link active" href="/">
+                        <NavLink className="nav-item nav-link" to="/" exact>
                             <i className="fas fa-home"></i> Home <span className="sr-only">(current)</span>
-                        </a>
-                        <a className="nav-item nav-link" href="/dashboard">
+                        </NavLink>
+                        <NavLink className="nav-item nav-link" to="/dashboard" exact>
                             <i className="fas fa-tachometer-alt"></i> Dashboard
-                        </a>
-                        <a className="nav-item nav-link" href="/search">
+                        </NavLink>
+                        <NavLink className="nav-item nav-link" to="/search" exact>
                             <i className="fas fa-film"></i> Search
-                        </a>
-                        <a className="nav-item nav-link" href="/api/logout">
+                        </NavLink>
+                        <NavLink className="nav-item nav-link" to="/api/logout" exact>
                         <i class="fas fa-sign-out-alt"></i> Logout
-                        </a>
+                        </NavLink>
                     </div>
                 </div>
             </nav>
