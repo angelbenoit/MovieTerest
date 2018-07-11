08f4906f948622f65e5cb3264db2eba51d8598d9
Angel Contreras <angelcbenoit@gmail.com>
2018-07-10 19:30:36 -0700

    heroku push

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const keys = require('./config/keys.js');
+const key = require('./config/key.js');
 const bodyParser = require('body-parser');
 
 require('./models/User.js');
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys:['fdfvfgnhrfb']
+    key:['fdfvfgnhrfb']
 }));
 
 app.use(passport.initialize());
@@ -21,5 +21,20 @@ app.use(passport.session());
 app.use(bodyParser());
 
 require('./routes/authRoutes.js')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
