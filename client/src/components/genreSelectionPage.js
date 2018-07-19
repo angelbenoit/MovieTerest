fed9de6d9a03edfd11c23cd5f39129e336b95526
Angel Contreras <angelcbenoit@gmail.com>
2018-07-18 21:02:53 -0700

    user can select multiple genres and will be highlighted

--- client/src/components/genreSelectionPage.js
+++ client/src/components/genreSelectionPage.js
@@ -4,6 +4,15 @@ import { connect } from 'react-redux';
 import * as actions from '../Actions';
 
 class genreSelectionPage extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selectedGenres: []
+        }
+        this.updateGenres = this.updateGenres.bind(this);
+        this.updateStyle = this.updateStyle.bind(this);
+    }
     componentWillMount() {
         this.props.resetPopular();
         this.props.fetchGenreList(this.props.match.params.format);
@@ -12,10 +21,28 @@ class genreSelectionPage extends Component {
     displayGenreList(){
         let list = this.props.genreList.map(genre => {
             return (
-                <div>{genre.name}</div>
+                <div
+                    className={genre.name}
+                    onClick={() => this.updateGenres(genre)}
+                    style={this.updateStyle(genre.id)}
+                >
+                    {genre.name}
+                </div>
             )
         });
         return list;
+    }
+
+    updateStyle(genreID){
+        for(let i = 0; i < this.state.selectedGenres.length; i++){
+            if(this.state.selectedGenres[i] === genreID)
+                return {border: '5px solid pink'};
+        }
+        return {border: '5px solid blue'};
+    }
+
+    updateGenres(genre){
+        this.setState({ selectedGenres: this.state.selectedGenres.concat(genre.id)}, () => console.log(this.state.selectedGenres));
     }
     render() {
         const genres = this.displayGenreList(this.props.genreList);
